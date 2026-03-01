434122b4e0f9957296f722ef891e251a50602407
roshanratansai <rr3538@srmist.edu.in>
2026-03-01 22:49:29 +0530

    UC3 - Refactored banner using String.join()

---
+++ UC3OOPSBannerApp.java
@@ -0,0 +1,22 @@
+public class UC3OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        String line1 = String.join(" ",
+                " ***** ", " ***** ", " ***** ", " ***** ");
+
+        String line2 = String.join(" ",
+                "*     *", "*     *", " *****", "  *****");
+
+        String line3 = String.join(" ",
+                "*     *", "*     *", " *     ", "     *");
+
+        String line4 = String.join(" ",
+                " ***** ", " ***** ", " * ", "     ***** ");
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+    }
+}
